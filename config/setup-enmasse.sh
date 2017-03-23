f73aff42b2de3307d6822b0c9ffcdf1a2ce5325a
Ulf Lilleengen <lulf@redhat.com>
2017-03-23 19:57:36 +0100

    Update deployment script to use stable release

--- config/setup-enmasse.sh
+++ config/setup-enmasse.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Step 1: Generate cert
-openssl req -new -x509 -batch -nodes -out server-cert.pem -keyout server-key.pem
+#openssl req -new -x509 -batch -nodes -out server-cert.pem -keyout server-key.pem
 
 # Step 2: Deploy EnMasse
-curl -o /tmp/enmasse-deploy.sh https://raw.githubusercontent.com/EnMasseProject/openshift-configuration/master/scripts/enmasse-deploy.sh 
-bash /tmp/enmasse-deploy.sh -u iot -p enmasse -s server-cert.pem -k server-key.pem -c https://awesome.iot-playground.org
+curl -o /tmp/enmasse-deploy.sh https://github.com/EnMasseProject/enmasse/releases/download/0.5/enmasse-deploy.sh
+bash /tmp/enmasse-deploy.sh -u iot -p enmasse -s server-cert.pem -k server-key.pem -c https://awesome.iot-playground.org -t https://github.com/EnMasseProject/enmasse/releases/download/0.5/tls-enmasse-template.yaml
