7cb5b5b6cc5934385573d1f00b92a57d71663898
Ulf Lilleengen <lulf@redhat.com>
2017-03-23 19:59:27 +0100

    Commit deploy script and template

--- config/setup-enmasse.sh
+++ config/setup-enmasse.sh
@@ -4,5 +4,4 @@
 #openssl req -new -x509 -batch -nodes -out server-cert.pem -keyout server-key.pem
 
 # Step 2: Deploy EnMasse
-curl -o /tmp/enmasse-deploy.sh https://github.com/EnMasseProject/enmasse/releases/download/0.5/enmasse-deploy.sh
-bash /tmp/enmasse-deploy.sh -u iot -p enmasse -s server-cert.pem -k server-key.pem -c https://awesome.iot-playground.org -t https://github.com/EnMasseProject/enmasse/releases/download/0.5/tls-enmasse-template.yaml
+bash enmasse-deploy.sh -u iot -p enmasse -s server-cert.pem -k server-key.pem -c https://awesome.iot-playground.org -t tls-enmasse-template.yaml
